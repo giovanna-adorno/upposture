b6aa967d0e4c4b4f69ba572031a302b2158d688e
giovanna <giovannacaroline135@gmail.com>
2021-09-12 12:46:51 -0300

    Add Controllers to Daily Summary And Posture

--- src/main/java/br/com/fiap/upposture/controller/api/DailySummaryController.java
+++ src/main/java/br/com/fiap/upposture/controller/api/DailySummaryController.java
@@ -1,15 +1,92 @@
 package br.com.fiap.upposture.controller.api;
 
+import br.com.fiap.upposture.model.DailySummary;
+import br.com.fiap.upposture.model.User;
 import br.com.fiap.upposture.repository.DailySummaryRepository;
+import br.com.fiap.upposture.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/daily-summary")
+@RequestMapping("/users/{userId}/daily-summaries")
 public class DailySummaryController {
 
     @Autowired
-    private DailySummaryRepository repository;
+    private DailySummaryRepository dailySummaryRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public Page<DailySummary> findAll(@PathVariable Long userId, @PageableDefault(size = 20) Pageable pageable) {
+        return dailySummaryRepository.findAllByUser_Id(userId, pageable);
+    }
+
+    @PostMapping
+    public ResponseEntity<DailySummary> create(@PathVariable Long userId, @RequestBody @Valid DailySummary dailySummary, UriComponentsBuilder uriBuilder) {
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isEmpty()){
+             return ResponseEntity.notFound().build();
+        }
+
+        dailySummary.setUser(user.get());
+
+        dailySummaryRepository.save(dailySummary);
+
+        URI uri = uriBuilder
+                .path("/users/{userId}/daily-summaries/{id}")
+                .buildAndExpand(userId, dailySummary.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(dailySummary);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<DailySummary> findOne(@PathVariable Long userId, @PathVariable Long id) {
+        return ResponseEntity.of(dailySummaryRepository.findByUser_IdAndId(userId, id));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<DailySummary> delete(@PathVariable Long userId, @PathVariable Long id){
+        Optional<DailySummary> dailySummary = dailySummaryRepository.findByUser_IdAndId(userId, id);
+
+        if(dailySummary.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        dailySummaryRepository.deleteById(id);
+
+        return ResponseEntity.ok().build();
+
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<DailySummary> update(@PathVariable Long userId, @RequestBody @Valid DailySummary newDailySummary, @PathVariable Long id ) {
+        Optional<DailySummary> optional = dailySummaryRepository.findByUser_IdAndId(userId, id);
+
+        if (optional.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        DailySummary dailySummary = optional.get();
+
+        dailySummary.setAlertPosture(newDailySummary.getAlertPosture());
+        dailySummary.setCorrectPosture(newDailySummary.getCorrectPosture());
+        dailySummary.setWrongPosture(newDailySummary.getWrongPosture());
+        dailySummary.setDate(newDailySummary.getDate());
+
+        dailySummaryRepository.save(dailySummary);
+
+        return ResponseEntity.ok(dailySummary);
+    }
 
 }
--- src/main/java/br/com/fiap/upposture/repository/PostureRepository.java
+++ src/main/java/br/com/fiap/upposture/repository/PostureRepository.java
@@ -1,7 +1,13 @@
 package br.com.fiap.upposture.repository;
 
+import br.com.fiap.upposture.model.PositionEnum;
 import br.com.fiap.upposture.model.Posture;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostureRepository extends JpaRepository<Posture, Long> {
+
+    Page<Posture> findByPosition(PositionEnum position, Pageable pageable);
+
 }
