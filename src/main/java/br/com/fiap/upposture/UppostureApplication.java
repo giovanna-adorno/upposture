bff0c4332f3d83ab3307152059a3d30d434a1e12
giovanna <giovannacaroline135@gmail.com>
2021-09-12 09:42:13 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/upposture/UppostureApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.upposture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UppostureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UppostureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/upposture/UppostureApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.upposture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UppostureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
